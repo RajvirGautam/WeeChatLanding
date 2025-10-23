9af70b8f474ae2ed1987c52200eb09b86107a5e0
RajvirGautam <rajvirgautam8855@gmail.com>
2025-10-23 15:46:06 +0530

    uploaded files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WeeChatLanding from './WeeChatLanding.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WeeChatLanding />
+  </React.StrictMode>,
+)
